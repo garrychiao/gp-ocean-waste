5cdc0a6546c291510acdb94c02877706175b2447
DavisLin <ylsh10309@gmail.com>
2019-06-22 13:21:42 +0800

    let App.vue cleaner

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-main>Main</el-main>
-      <el-aside width="200px">NavBar</el-aside>
-    </el-container>
+    <OceanMain></OceanMain>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  components: {
-  }
+  name: 'app'
 }
 </script>
 
@@ -26,20 +21,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-.el-aside {
-  background-color: #D3DCE6;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
-  height: 100vh;
-}
-.el-main {
-  background-color: #E9EEF3;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
-  height: 100vh;
 }
 </style>
---
+++ src/views/OceanMain.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-container>
+      <el-main>Main</el-main>
+      <el-aside width="10em">NavBar</el-aside>
+  </el-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+/deep/ .el-aside {
+  background-color: #2F3B46;
+  color: #FFF;
+  height: 100%;
+  width: 10em;
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9999;
+}
+/deep/ .el-main {
+  padding-right: 10em;
+  position: relative;
+  z-index: 1;
+  height: 100vh;
+  padding: 0;
+}
+</style>
