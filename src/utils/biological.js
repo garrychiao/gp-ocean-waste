747fc269f1947876a68d1ebc3c5ae7c513c73e2c
DavisLin <ylsh10309@gmail.com>
2019-07-16 17:45:57 +0800

    add biological info and update big data page

---
+++ src/utils/biological.js
@@ -0,0 +1,46 @@
+export const biologicalImpact = [
+  {
+    name: 'GP0STSIIB',
+    content: '綠色和平船艦極地曙光號造訪太平洋大垃圾帶時，在漁網垃圾裡紀錄到螃蟹身上帶著微塑膠。',
+    url: require('@/assets/images/GP0STSIIB.jpg')
+  },
+  {
+    name: 'GP0WB0',
+    content: '雀鯛把寶特瓶當成自己的家，護衛著地盤。這次在環臺海岸快篩中發現，不論東南西北，寶特瓶是臺灣海岸上最常發現的垃圾。',
+    url: require('@/assets/images/GP0WB0.jpg')
+  },
+  {
+    name: 'GP0CUJ',
+    content: '一隻海馬在塑膠袋裡偽裝自己。這次快篩調查也發現，離景點越近的海岸上越容易發現塑膠袋。',
+    url: require('@/assets/images/GP0CUJ.jpg')
+  },
+  {
+    name: 'GP0STT3FM',
+    content: '螃蟹困在手搖杯裡。研究人員發現，小型塑膠製品多半是來自都市的海廢垃圾。',
+    url: require('@/assets/images/GP0STT3FM.jpg')
+  },
+  {
+    name: 'GP0STQ7ON',
+    content: '桌型軸孔珊瑚上纏掛著塑膠袋。科學家認為，珊瑚被塑膠纏上，就像被一把很髒的刀子割過，使珊瑚容易染病死亡。',
+    url: require('@/assets/images/GP0STQ7ON.jpg')
+  },
+  {
+    name: 'GP0STSJ0S',
+    content: '在太平洋大垃圾帶發現的黃色浮球，已經成為鵝頸藤壼和螃蟹的家，甚至還吸引了一小群慈鯛。',
+    url: require('@/assets/images/GP0STSJ0S.jpg')
+  },
+  {
+    name: 'GP0STQV74',
+    content: '蘇格蘭的希恩特群島是眾多海鳥的家，但就連這裡，也發現海鸚咬食漁網的碎片。科學家估計，到2050年，99%的海鳥都會吃進塑膠垃圾。',
+    url: require('@/assets/images/GP0STQV74.jpg')
+  },
+  {
+    name: 'GP0STRVGA',
+    content: '在馬來西亞著名印度教景點雪蘭莪，發現猴子從遊客留下的塑膠垃圾裡找東西吃。只有我們減少使用，才不會危害這些動物。',
+    url: require('@/assets/images/GP0STRVGA.jpg')
+  },
+]
+
+export default {
+  biologicalImpact
+}
--- src/views/SessionBigData.vue
+++ src/views/SessionBigData.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="big-data">
-    <image-compare
-      :before="right"
-      :after="left"
-      :padding="{ left: 50, right: 50 }"
-    >
+    <el-carousel :autoplay="false" arrow="hover" indicator-position="none" height="500px">
+      <el-carousel-item v-for="item in biological" :key="item.name">
+        <div class="biological">
+          <div class="biological__image">
+            <el-image
+              style="height: inherit"
+              :src="item.url"
+              fit="cover"></el-image>
+          </div>
+          <div class="biological__content">
+            <span>{{ item.content }}</span>
+          </div>
+        </div>   
+      </el-carousel-item>
+    </el-carousel>
+    <el-card shadow="never">
+      <image-compare
+        :before="right"
+        :after="left"
+        :padding="{ left: 50, right: 50 }"
+      >
       <template v-slot:icon-left>
         <i class="el-icon-caret-left"></i>
       </template>
@@ -12,15 +28,22 @@
         <i class="el-icon-caret-right"></i>
       </template>
     </image-compare>
+    </el-card>
   </div>
 </template>
 
 <script>
+import { biologicalImpact } from '@/utils/biological'
 export default {
-    data: () =>({
-    left: require('../assets/left.jpg'),
-    right: require('../assets/right.jpg')
-  })
+  data: () =>({
+    left: require('@/assets/images/left.jpg'),
+    right: require('@/assets/images/right.jpg')
+  }),
+  computed: {
+    biological () {
+      return biologicalImpact
+    }
+  }
 }
 </script>
 
@@ -29,9 +52,32 @@ export default {
     height: initial;
     width: 100vw;
   }
-    /deep/ .image-compare-handle-icon {
+  /deep/ .image-compare-handle-icon {
     &.left, &.right {
       padding: 0 !important
     }
   }
+  .biological {
+    width: inherit;
+    height: inherit;
+    display: flex;
+    &__image {
+      width: 70%;
+      height: inherit;
+    }
+    &__content {
+      width: 30%;
+      height: 480px;
+      padding: 10px;
+      display: flex;
+      align-items: flex-start;
+      justify-content: center;
+      color: #fff;
+      text-align: left;
+      font-size: 20px;
+      background-color: #2F3B46;
+      border-left: 2px solid #e84118;
+    }
+  }
+  
 </style>
